a98432fd3dc0d1ac83a615762edf8c0e8d116bb1
0092Nasir <101786736+0092Nasir@users.noreply.github.com>
2024-11-30 14:02:35 +0000

    iteration

---
+++ 05_iterations/four.js
@@ -0,0 +1,28 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(programming[key]);
+// if we directly paas the key variable it will return the keys of the arraytype.
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+// forin loop will not work in map object but does'nt throw error also.
